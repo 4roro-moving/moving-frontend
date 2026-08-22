49eeea0ed6a924cd6ea5a9cd12143644ef034fc1
9g-g9 <nayun12375@gmail.com>
2026-08-22 20:46:53 +0900

    feat: list card 신청 건 수 조회 추가, 내가 작성한 나눔 신청 글 bg color 변경

--- src/components/giveaway/GiveawayCardSkeletonList.tsx
+++ src/components/giveaway/GiveawayCardSkeletonList.tsx
@@ -12,9 +12,12 @@ const GiveawayCardSkeletonList = ({ count = 8 }: GiveawayCardSkeletonListProps) 
           <li key={index}>
             <div className="bg-background-default border-border-subtle shadow-estimate-card rounded-20 flex flex-col gap-20 border-[0.5px] p-40">
               <Skeleton className="rounded-6 h-[219px] w-full" />
-              <div className="flex flex-col items-center gap-12">
-                <Skeleton className="h-32 w-4/5" />
-                <Skeleton className="h-24 w-80 self-start" />
+              <div className="flex flex-col gap-12">
+                <Skeleton className="h-32 w-4/5 self-center" />
+                <div className="flex w-full items-center justify-between">
+                  <Skeleton className="h-24 w-80" />
+                  <Skeleton className="h-24 w-40" />
+                </div>
               </div>
             </div>
           </li>
