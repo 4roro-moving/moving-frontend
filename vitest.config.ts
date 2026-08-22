fb30e4724a4c495a017ec3b7343e215cb5974e0a
yooseohyeon <yooseohyeon.02@gmail.com>
2026-08-22 13:12:12 +0900

    test:  testing-library/react 설치 등 훅 테스트 환경 구성

--- vitest.config.ts
+++ vitest.config.ts
@@ -12,8 +12,26 @@ const dirname =
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.join(dirname, "src"),
+    },
+  },
   test: {
     projects: [
+      {
+        extends: true,
+        test: {
+          name: "unit",
+          include: ["src/**/*.test.{ts,tsx}"],
+          browser: {
+            enabled: true,
+            headless: true,
+            provider: playwright({}),
+            instances: [{ browser: "chromium" }],
+          },
+        },
+      },
       {
         extends: true,
         plugins: [
